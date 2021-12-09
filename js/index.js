e806c1ccb78a916d9661857f8706f07876c0169c
CHENC <2499443165@qq.com>
2021-12-09 15:09:34 +0800

    内容页面

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function() {
     $("#dianji").on("click", "li", function() {
-        $(".news div").eq($(this).index()).removeClass("none").addClass("block").siblings("div").addClass("none")
+        $(".news").children().eq($(this).index()).removeClass("none").addClass("block").siblings("div").addClass("none")
         $(this).addClass("active").siblings().removeClass("active")
     })
 })
