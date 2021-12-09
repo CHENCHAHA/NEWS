a1aed4bf5862243194a0a01fff565d4b50db38ff
CHENC <2499443165@qq.com>
2021-12-09 12:54:17 +0800

    样式修改

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(function() {
     $("#dianji").on("click", "li", function() {
         $(".news div").eq($(this).index()).removeClass("none").addClass("block").siblings("div").addClass("none")
+        $(this).addClass("active").siblings().removeClass("active")
     })
 })
