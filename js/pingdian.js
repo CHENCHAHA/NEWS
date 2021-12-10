d7b16842a6d4955bb7db3f4aa61dd035d542562a
CHENC <2499443165@qq.com>
2021-12-10 10:41:35 +0800

    提交最终代码

--- js/pingdian.js
+++ js/pingdian.js
@@ -2,10 +2,21 @@
  * 这是处理评论点赞的js
  */
 $(function() {
+    var f = true
     $(".dian").on("click", function() {
-        var zan = $(".dianzan").text()
-        zan++
-        $(".dianzan").text(zan)
-        alert("点赞成功")
+        if (f) {
+            var zan = $(".dianzan").text()
+            zan++
+            $(".dianzan").text(zan)
+            f = false
+            alert("你已经点过赞了")
+        }
+
+    })
+    $(".fa").on("click", function() {
+        var liu = $(".inp").val()
+        console.log(liu);
+        var xing = $('<li>陈超:' + liu + '</li>')
+        $("ul").prepend(xing)
     })
 })
